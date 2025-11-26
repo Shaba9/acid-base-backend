aeedca8930e3d5cccf33bf3f41b254e8ff82b6e9
srahavi <srahavi@huronconsultinggroup.com>
2025-11-26 13:53:55 -0800

    Adjust ranges

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_ph(bicarbonate: float = Query(...), pco2: float = Query(...)):
 @app.get("/matching-combos")
 def get_matching_combos(pH: float = Query(...)) -> Dict[str, List[Dict[str, float]]]:
     combos = []
-    for b in range(10, 41):  # bicarbonate range
+    for b in range(21, 27):  # bicarbonate range
         for vco2 in range(100, 1201, 100):  # CO2 production range
             for va in range(1, 16):  # alveolar ventilation range
                 pco2 = (vco2 * 0.863) / va
